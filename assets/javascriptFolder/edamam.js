cb7896713bfa09e21da99b0860b03ebaae3d41a7
Charles Molitor <Charlie@Charless-MacBook-Pro.local>
2022-07-26 19:52:36 -0700

    Starting to add ids to html file and variables to js

--- assets/javascriptFolder/edamam.js
+++ assets/javascriptFolder/edamam.js
@@ -1,12 +1,35 @@
-console.log('hello');
 var appid = '&app_id=e6094091';
 var appkey ='&app_key=998bd7f197846f5a6ef876a82bf64a36';
 var edamamurl = 'https://api.edamam.com/search?q=oats&app_id=e6094091&app_key=9885703d5ed639adced50ead66ba1a1c&mealtype=lunch&calories=500-700';
-var searchid = 'chicken';
 
-var q = '';
-var totalurl = edamamurl + searchid + appid + appkey;
-console.log(totalurl);
+var baseUrl = 'https://api.edamam.com/search?';
+var urlKeys = '&app_id=e6094091&app_key=9885703d5ed639adced50ead66ba1a1c';
+
+//This will be the q= parameter
+var baseBreakfast;
+var baseLunch;
+var baseDinner;
+
+//These will be checkboxes parameters
+var calorieBreakfast;
+var calorieLunch;
+var calorieDinner;
+
+//These will be parameter restrictions
+var vegetarian;
+var vegan;
+
+//These will be allergy restrictions
+var gluten;
+var dairy;
+var peanut;
+
+
+
+
+
+
+
 
 function getApi() {
     fetch(edamamurl)
