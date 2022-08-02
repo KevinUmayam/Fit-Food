eab84d0d3eaef7f5608a550145f208fb322ee8f5
CMolitor96 <103666997+CMolitor96@users.noreply.github.com>
2022-08-02 14:30:44 -0700

    Update calendar.js

---
+++ assets/javascriptFolder/calendar.js
@@ -0,0 +1,53 @@
+var url = new URL(window.location);
+var params = new URLSearchParams(url.search);
+var breakfastName = (params.getAll('breakfastName'));
+var breakfastUrl = (params.getAll('breakfastUrl'));
+var lunchName = (params.getAll('lunchName'));
+var lunchUrl = (params.getAll('lunchUrl'));
+var dinnerName = (params.getAll('dinnerName'));
+var dinnerUrl = (params.getAll('dinnerUrl'));
+
+// console.log(goose);
+// console.log(fox);
+
+// console.log(goose.length);
+// console.log(fox.length);
+console.log(breakfastName.length);
+console.log(lunchName.length);
+console.log(dinnerName.length);
+
+meal(breakfastName, breakfastUrl, 'b') //breakfast();
+meal(lunchName, lunchUrl, 'l') //lunch();
+meal(dinnerName, dinnerUrl, 'd')// dinner();
+
+function meal(name, url, letter) {
+    if (name.length === 0) { return; } 
+    for (var i = 0; i < 7; i++) {
+        var meal = document.getElementsByClassName(letter + '-ttl');
+        meal[i].textContent = name[i];
+        var recipe = document.getElementsByClassName(letter + '-btn');
+        var button = document.createElement('button');
+        button.textContent = 'Click for recipe';
+        var aMeal = document.createElement('a');
+        aMeal.href = url[i];
+        aMeal.appendChild(button);
+        recipe[i].appendChild(aMeal);
+    }
+}
+
+function breakfast() {
+    if (breakfastName.length === 0) {
+        return;
+    } else {
+
+        for (var i = 0; i < 7; i++) {
+            var breakfast = document.getElementsByClassName('b-ttl');
+            breakfast[i].textContent = breakfastName[i];
+            var breakfastRecipe = document.getElementsByClassName('b-btn');
+            var breakfastButton = document.createElement('button');
+            breakfastButton.textContent = 'Click for recipe';
+            var aBreakfast = document.createElement('a');
+            aBreakfast.href = breakfastUrl[i];
+            aBreakfast.appendChild(breakfastButton);
+            breakfastRecipe[i].appendChild(aBreakfast);
+        }
